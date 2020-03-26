23d7ade5e09a359055e61e1b5921acabf5b31c4e
exalcams <exalca.plant1@gmail.com>
2020-03-26 19:45:37 +0530

    Initial Check In

---
+++ src/app/allModules/pages/pages.module.ts
@@ -0,0 +1,128 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FuseSidebarModule } from '@fuse/components';
+
+import {
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule
+} from '@angular/material';
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import {
+    FuseCountdownModule,
+    FuseHighlightModule,
+    FuseMaterialColorPickerModule,
+    FuseWidgetModule
+} from '@fuse/components';
+
+import { FuseSharedModule } from '@fuse/shared.module';
+import { FormsModule } from '@angular/forms';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DecimalPipe } from '@angular/common';
+
+const routes = [
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/auth/login'
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes),
+        // HttpClientModule,
+        // TranslateModule,
+        MatFormFieldModule,
+        MatAutocompleteModule,
+        MatBadgeModule,
+        MatBottomSheetModule,
+        MatButtonModule,
+        MatButtonToggleModule,
+        MatCardModule,
+        MatCheckboxModule,
+        MatChipsModule,
+        MatStepperModule,
+        MatDatepickerModule,
+        MatDialogModule,
+        MatDividerModule,
+        MatExpansionModule,
+        MatGridListModule,
+        MatIconModule,
+        MatInputModule,
+        MatListModule,
+        MatMenuModule,
+        MatNativeDateModule,
+        MatPaginatorModule,
+        MatProgressBarModule,
+        MatProgressSpinnerModule,
+        MatRadioModule,
+        MatRippleModule,
+        MatSelectModule,
+        MatSidenavModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatSnackBarModule,
+        MatSortModule,
+        MatTableModule,
+        MatTabsModule,
+        MatToolbarModule,
+        MatTooltipModule,
+        MatTreeModule,
+
+        NgxChartsModule,
+
+        FuseSharedModule,
+        FuseSidebarModule,
+
+        FuseCountdownModule,
+        FuseHighlightModule,
+        FuseMaterialColorPickerModule,
+        FuseWidgetModule,
+
+        FormsModule
+    ],
+    declarations: [DashboardComponent],
+    providers: [
+        DecimalPipe
+    ],
+    entryComponents: []
+})
+export class PagesModule { }
