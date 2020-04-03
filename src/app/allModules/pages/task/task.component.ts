b934b55f847ca8ddaba7a58cf700797561f15092
exalcams <exalca.plant1@gmail.com>
2020-04-03 15:49:00 +0530

    task screen continues..

--- src/app/allModules/pages/task/task.component.ts
+++ src/app/allModules/pages/task/task.component.ts
@@ -25,6 +25,7 @@ export class TaskComponent implements OnInit {
   AllMenuApps: MenuApp[] = [];
   SelectedMenuApp: MenuApp;
   authenticationDetails: AuthenticationDetails;
+  CurrentUserRole = '';
   notificationSnackBarComponent: NotificationSnackBarComponent;
   IsProgressBarVisibile: boolean;
   taskFormGroup: FormGroup;
@@ -41,30 +42,34 @@ export class TaskComponent implements OnInit {
   OutputsByTask: Output[] = [];
   LogicsByTask: Logic[] = [];
   ValidationsByTask: Validation[] = [];
-  inputdisplayedColumns: string[] = [
+  InputdisplayedColumns: string[] = [
+    'Action',
     'Field',
     'Validation',
     'Remarks'
   ];
-  outputdisplayedColumns: string[] = [
+  OutputdisplayedColumns: string[] = [
+    'Action',
     'Level',
     'Field',
     'Validation',
     'Remarks'
   ];
-  logicdisplayedColumns: string[] = [
+  LogicdisplayedColumns: string[] = [
+    'Action',
     'LogicText'
   ];
-  validationdisplayedColumns: string[] = [
+  ValidationdisplayedColumns: string[] = [
+    'Action',
     'ValidationText'
   ];
   sketchdisplayedColumns: string[] = [
     'Text'
   ];
-  InputdataSource = new MatTableDataSource<any>();
-  OutputdataSource = new MatTableDataSource<any>();
-  LogicdataSource = new MatTableDataSource<any>();
-  ValidationdataSource = new MatTableDataSource<any>();
+  InputdataSource = new MatTableDataSource<Input>();
+  OutputdataSource = new MatTableDataSource<Output>();
+  LogicdataSource = new MatTableDataSource<Logic>();
+  ValidationdataSource = new MatTableDataSource<Validation>();
   sketchdataSource = new MatTableDataSource<any>();
   selection = new SelectionModel<any>(true, []);
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -88,8 +93,9 @@ export class TaskComponent implements OnInit {
     const retrievedObject = localStorage.getItem('authorizationData');
     if (retrievedObject) {
       this.authenticationDetails = JSON.parse(retrievedObject) as AuthenticationDetails;
+      this.CurrentUserRole = this.authenticationDetails.userRole;
       this.MenuItems = this.authenticationDetails.menuItemNames.split(',');
-      if (this.MenuItems.indexOf('User') < 0) {
+      if (this.MenuItems.indexOf('Task') < 0) {
         this.notificationSnackBarComponent.openSnackBar('You do not have permission to visit this page', SnackBarStatus.danger);
         this._router.navigate(['/auth/login']);
       }
@@ -102,44 +108,12 @@ export class TaskComponent implements OnInit {
     } else {
       this._router.navigate(['/auth/login']);
     }
-    const allInvoiceDetails = [
-      { Field: '', Validation: '', Remarks: '' },
-      { Field: '', Validation: '', Remarks: '' },
-      { Field: '', Validation: '', Remarks: '' }
-    ];
-    const allOutputDetails = [
-      { Level: '', Field: '', Validation: '', Remarks: '' },
-      { Level: '', Field: '', Validation: '', Remarks: '' },
-      { Level: '', Field: '', Validation: '', Remarks: '' }
-    ];
-    const allLogicDetails = [
-      { LogicText: '' },
-      { LogicText: '' },
-      { LogicText: '' },
-    ];
-    const allValidationDetails = [
-      { ValidationText: '' },
-      { ValidationText: '' },
-      { ValidationText: '' },
-    ];
     const allSketchDetails = [
       { Text: '' },
       { Text: '' },
       { Text: '' },
     ];
 
-    this.InputdataSource = new MatTableDataSource(
-      allInvoiceDetails
-    );
-    this.OutputdataSource = new MatTableDataSource(
-      allOutputDetails
-    );
-    this.LogicdataSource = new MatTableDataSource(
-      allLogicDetails
-    );
-    this.ValidationdataSource = new MatTableDataSource(
-      allValidationDetails
-    );
     this.sketchdataSource = new MatTableDataSource(
       allSketchDetails
     );
@@ -152,9 +126,24 @@ export class TaskComponent implements OnInit {
       EstimatedEffort: ['', Validators.required],
       CompletionBefore: ['', Validators.required],
       AssignedTo: ['', Validators.required],
-      AcceptedEffort: ['', Validators.required],
-      AcceptedCompletionDate: ['', Validators.required]
+      AcceptedEffort: [''],
+      AcceptedCompletionDate: ['']
     });
+    this.DynamicallyAddAcceptedValidation();
+  }
+
+  DynamicallyAddAcceptedValidation(): void {
+    if (this.CurrentUserRole.toLocaleLowerCase() === 'developer') {
+      this.taskFormGroup.get('AcceptedEffort').setValidators(Validators.required);
+      this.taskFormGroup.get('AcceptedEffort').updateValueAndValidity();
+      this.taskFormGroup.get('AcceptedCompletionDate').setValidators(Validators.required);
+      this.taskFormGroup.get('AcceptedCompletionDate').updateValueAndValidity();
+    } else {
+      this.taskFormGroup.get('AcceptedEffort').clearValidators();
+      this.taskFormGroup.get('AcceptedEffort').updateValueAndValidity();
+      this.taskFormGroup.get('AcceptedCompletionDate').clearValidators();
+      this.taskFormGroup.get('AcceptedCompletionDate').updateValueAndValidity();
+    }
   }
 
   InitializeInputFormGroup(): void {
@@ -195,6 +184,59 @@ export class TaskComponent implements OnInit {
       this.taskFormGroup.get(key).markAsUntouched();
     });
     // this.fileToUpload = null;
+    this.ClearInputFormGroup();
+    this.ClearOutputFormGroup();
+    this.ClearLogicFormGroup();
+    this.ClearValidationFormGroup();
+    this.ClearInputDataSource();
+    this.ClearOutputDataSource();
+    this.ClearLogicDataSource();
+    this.ClearValidationDataSource();
+  }
+
+  ClearInputFormGroup(): void {
+    this.inputFormGroup.reset();
+    Object.keys(this.inputFormGroup.controls).forEach(key => {
+      this.inputFormGroup.get(key).markAsUntouched();
+    });
+  }
+
+  ClearInputDataSource(): void {
+    this.InputsByTask = [];
+    this.InputdataSource = new MatTableDataSource(this.InputsByTask);
+  }
+
+  ClearOutputFormGroup(): void {
+    this.outputFormGroup.reset();
+    Object.keys(this.outputFormGroup.controls).forEach(key => {
+      this.outputFormGroup.get(key).markAsUntouched();
+    });
+
+  }
+  ClearOutputDataSource(): void {
+    this.OutputsByTask = [];
+    this.OutputdataSource = new MatTableDataSource(this.OutputsByTask);
+  }
+
+  ClearLogicFormGroup(): void {
+    this.logicFormGroup.reset();
+    Object.keys(this.logicFormGroup.controls).forEach(key => {
+      this.logicFormGroup.get(key).markAsUntouched();
+    });
+  }
+  ClearLogicDataSource(): void {
+    this.LogicsByTask = [];
+    this.LogicdataSource = new MatTableDataSource(this.LogicsByTask);
+  }
+  ClearValidationFormGroup(): void {
+    this.validationFormGroup.reset();
+    Object.keys(this.validationFormGroup.controls).forEach(key => {
+      this.validationFormGroup.get(key).markAsUntouched();
+    });
+  }
+  ClearValidationDataSource(): void {
+    this.ValidationsByTask = [];
+    this.ValidationdataSource = new MatTableDataSource(this.ValidationsByTask);
   }
 
   GetAllTasks(): void {
@@ -231,7 +273,7 @@ export class TaskComponent implements OnInit {
     this.taskFormGroup.get('Type').patchValue(this.SelectedTask.Type);
     this.taskFormGroup.get('EstimatedEffort').patchValue(this.SelectedTask.EstimatedEffort);
     this.taskFormGroup.get('CompletionBefore').patchValue(this.SelectedTask.CompletionBefore);
-    this.taskFormGroup.get('contactNumber').patchValue(this.SelectedTask.AssignedTo);
+    this.taskFormGroup.get('AssignedTo').patchValue(this.SelectedTask.AssignedTo);
     this.taskFormGroup.get('AcceptedEffort').patchValue(this.SelectedTask.AcceptedEffort);
     this.taskFormGroup.get('AcceptedCompletionDate').patchValue(this.SelectedTask.AcceptedCompletionDate);
   }
@@ -308,47 +350,99 @@ export class TaskComponent implements OnInit {
   }
 
   AddInputToTable(): void {
-    const input = new Input();
-    input.Field = this.inputFormGroup.get('Field').value;
-    input.Validation = this.inputFormGroup.get('Validation').value;
-    input.Remarks = this.inputFormGroup.get('Remarks').value;
-    if (!this.InputsByTask || !this.InputsByTask.length) {
-      this.InputsByTask = [];
+    if (this.inputFormGroup.valid) {
+      const input = new Input();
+      input.Field = this.inputFormGroup.get('Field').value;
+      input.Validation = this.inputFormGroup.get('Validation').value;
+      input.Remarks = this.inputFormGroup.get('Remarks').value;
+      if (!this.InputsByTask || !this.InputsByTask.length) {
+        this.InputsByTask = [];
+      }
+      this.InputsByTask.push(input);
+      this.InputdataSource = new MatTableDataSource(this.InputsByTask);
+      this.ClearInputFormGroup();
+    } else {
+      this.ShowValidationErrors(this.inputFormGroup);
     }
-    this.InputsByTask.push(input);
-    this.InputdataSource = new MatTableDataSource(this.InputsByTask);
   }
 
   AddOutputToTable(): void {
-    const output = new Output();
-    output.Level = this.outputFormGroup.get('Level').value;
-    output.Field = this.outputFormGroup.get('Field').value;
-    output.Validation = this.outputFormGroup.get('Validation').value;
-    output.Remarks = this.outputFormGroup.get('Remarks').value;
-    if (!this.OutputsByTask || !this.OutputsByTask.length) {
-      this.OutputsByTask = [];
+    if (this.outputFormGroup.valid) {
+      const output = new Output();
+      output.Level = this.outputFormGroup.get('Level').value;
+      output.Field = this.outputFormGroup.get('Field').value;
+      output.Validation = this.outputFormGroup.get('Validation').value;
+      output.Remarks = this.outputFormGroup.get('Remarks').value;
+      if (!this.OutputsByTask || !this.OutputsByTask.length) {
+        this.OutputsByTask = [];
+      }
+      this.OutputsByTask.push(output);
+      this.OutputdataSource = new MatTableDataSource(this.OutputsByTask);
+      this.ClearOutputFormGroup();
+    } else {
+      this.ShowValidationErrors(this.outputFormGroup);
     }
-    this.OutputsByTask.push(output);
-    this.OutputdataSource = new MatTableDataSource(this.OutputsByTask);
   }
 
   AddLogicToTable(): void {
-    const logic = new Logic();
-    logic.LogicText = this.logicFormGroup.get('LogicText').value;
-    if (!this.LogicsByTask || !this.LogicsByTask.length) {
-      this.LogicsByTask = [];
+    if (this.logicFormGroup.valid) {
+      const logic = new Logic();
+      logic.LogicText = this.logicFormGroup.get('LogicText').value;
+      if (!this.LogicsByTask || !this.LogicsByTask.length) {
+        this.LogicsByTask = [];
+      }
+      this.LogicsByTask.push(logic);
+      this.LogicdataSource = new MatTableDataSource(this.LogicsByTask);
+      this.ClearLogicFormGroup();
+    } else {
+      this.ShowValidationErrors(this.logicFormGroup);
     }
-    this.LogicsByTask.push(logic);
-    this.LogicdataSource = new MatTableDataSource(this.LogicsByTask);
   }
 
   AddValidationToTable(): void {
-    const validation = new Validation();
-    validation.ValidationText = this.validationFormGroup.get('ValidationText').value;
-    if (!this.ValidationsByTask || !this.ValidationsByTask.length) {
-      this.ValidationsByTask = [];
+    if (this.validationFormGroup.valid) {
+      const validation = new Validation();
+      validation.ValidationText = this.validationFormGroup.get('ValidationText').value;
+      if (!this.ValidationsByTask || !this.ValidationsByTask.length) {
+        this.ValidationsByTask = [];
+      }
+      this.ValidationsByTask.push(validation);
+      this.ValidationdataSource = new MatTableDataSource(this.ValidationsByTask);
+      this.ClearValidationFormGroup();
+    } else {
+      this.ShowValidationErrors(this.validationFormGroup);
     }
-    this.ValidationsByTask.push(validation);
+
+  }
+
+  RemoveInputFromTable(input: Input): void {
+    const index: number = this.InputsByTask.indexOf(input);
+    if (index > -1) {
+      this.InputsByTask.splice(index, 1);
+    }
+    this.InputdataSource = new MatTableDataSource(this.InputsByTask);
+  }
+
+  RemoveOutputFromTable(output: Output): void {
+    const index: number = this.OutputsByTask.indexOf(output);
+    if (index > -1) {
+      this.OutputsByTask.splice(index, 1);
+    }
+    this.OutputdataSource = new MatTableDataSource(this.OutputsByTask);
+  }
+  RemoveLogicFromTable(logic: Logic): void {
+    const index: number = this.LogicsByTask.indexOf(logic);
+    if (index > -1) {
+      this.LogicsByTask.splice(index, 1);
+    }
+    this.LogicdataSource = new MatTableDataSource(this.LogicsByTask);
+  }
+
+  RemoveValidationFromTable(validation: Validation): void {
+    const index: number = this.ValidationsByTask.indexOf(validation);
+    if (index > -1) {
+      this.ValidationsByTask.splice(index, 1);
+    }
     this.ValidationdataSource = new MatTableDataSource(this.ValidationsByTask);
   }
 
@@ -378,6 +472,7 @@ export class TaskComponent implements OnInit {
   GetTaskValues(): void {
     this.SelectedTask.Title = this.SelectedTaskView.Title = this.taskFormGroup.get('Title').value;
     this.SelectedTask.Type = this.SelectedTaskView.Type = this.taskFormGroup.get('Type').value;
+    this.SelectedTask.EstimatedEffort = this.SelectedTaskView.EstimatedEffort = this.taskFormGroup.get('EstimatedEffort').value;
     this.SelectedTask.CompletionBefore = this.SelectedTaskView.CompletionBefore = this.taskFormGroup.get('CompletionBefore').value;
     this.SelectedTask.AssignedTo = this.SelectedTaskView.AssignedTo = this.taskFormGroup.get('AssignedTo').value;
     this.SelectedTask.AcceptedEffort = this.SelectedTaskView.AcceptedEffort = this.taskFormGroup.get('AcceptedEffort').value;
@@ -426,7 +521,7 @@ export class TaskComponent implements OnInit {
   CreateTask(): void {
     this.GetTaskValues();
     this.GetTaskSubItemValues();
-    this.SelectedTask.CreatedBy = this.authenticationDetails.userID.toString();
+    this.SelectedTaskView.CreatedBy = this.authenticationDetails.userID.toString();
     this.IsProgressBarVisibile = true;
     this._projectService.CreateTask(this.SelectedTaskView).subscribe(
       (data) => {
@@ -447,7 +542,9 @@ export class TaskComponent implements OnInit {
 
   UpdateTask(): void {
     this.GetTaskValues();
-    this.SelectedTask.ModifiedBy = this.authenticationDetails.userID.toString();
+    this.GetTaskSubItemValues();
+    this.SelectedTaskView.TaskID = this.SelectedTask.TaskID;
+    this.SelectedTaskView.ModifiedBy = this.authenticationDetails.userID.toString();
     this.IsProgressBarVisibile = true;
     this._projectService.UpdateTask(this.SelectedTaskView).subscribe(
       (data) => {
@@ -485,10 +582,10 @@ export class TaskComponent implements OnInit {
     );
   }
 
-  ShowValidationErrors(): void {
-    Object.keys(this.taskFormGroup.controls).forEach(key => {
-      this.taskFormGroup.get(key).markAsTouched();
-      this.taskFormGroup.get(key).markAsDirty();
+  ShowValidationErrors(formGroup: FormGroup): void {
+    Object.keys(formGroup.controls).forEach(key => {
+      formGroup.get(key).markAsTouched();
+      formGroup.get(key).markAsDirty();
     });
 
   }
@@ -506,7 +603,7 @@ export class TaskComponent implements OnInit {
         this.OpenConfirmationDialog(Actiontype, Catagory);
       }
     } else {
-      this.ShowValidationErrors();
+      this.ShowValidationErrors(this.taskFormGroup);
     }
   }
 
@@ -518,7 +615,7 @@ export class TaskComponent implements OnInit {
         this.OpenConfirmationDialog(Actiontype, Catagory);
       }
     } else {
-      this.ShowValidationErrors();
+      this.ShowValidationErrors(this.taskFormGroup);
     }
   }
 
