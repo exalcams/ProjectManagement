3c7e88e92004a2ad4d55d4ab5e93dd0d4b60b4b1
exalcams <exalca.plant1@gmail.com>
2020-04-03 19:02:24 +0530

    services address error..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,10 @@ export const environment = {
     production: true,
     hmr: false,
     // baseAddress : 'http://192.168.0.28:8001/',
+<<<<<<< HEAD
+=======
+    // baseAddress: 'http://10.111.0.124:8001/',
+>>>>>>> services address error..
     baseAddress: 'http://192.168.1.35/v1/',
     clientId: 'ngAuthApp'
 };
